084cd260c4df56c120838e506cee0d3718afed7d
unio <unioblog@gmail.com>
2018-12-06 02:48:02 +0900

    add build

---
+++ build/precache-manifest.8d346d5d8e92808fecc25d0f20bb8743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "3a374c1087399f4a9017",
+    "url": "/portfolio/static/js/main.3a374c10.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/portfolio/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "3a374c1087399f4a9017",
+    "url": "/portfolio/static/css/main.33cca75d.chunk.css"
+  },
+  {
+    "revision": "c92ad0fec2175e03656b1a0b526db968",
+    "url": "/portfolio/index.html"
+  }
+];
