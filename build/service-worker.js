17d3714aac2cab26beb30ae902bab06358c224f1
unio <unioblog@gmail.com>
2018-12-25 02:05:27 +0900

    title変更

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bffab792317c428ded7fe1b72fc9e35d.js"
+  "/portfolio/precache-manifest.10d9bb1f01e217f0c0da898bdd0b6d95.js"
 );
 
 workbox.clientsClaim();
