84809814c2458c7dabbdb65c2ceb875463d098f0
unio <unioblog@gmail.com>
2018-12-26 01:59:10 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c2ff929d78706de248d8d024b03a480b.js"
+  "/portfolio/precache-manifest.956ec86c62b39e4508fd3f46b1f6233f.js"
 );
 
 workbox.clientsClaim();
