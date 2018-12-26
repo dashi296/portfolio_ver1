0c9a1b4d95e0b636e7d807adb484f1863fa5c1f3
unio <unioblog@gmail.com>
2018-12-27 00:17:20 +0900

    Link to 修正

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.99e9c4ed602b68838d1abff85ad9f979.js"
+  "/portfolio/precache-manifest.beaa6a42c8b445e61f8a2fc2a5d2594d.js"
 );
 
 workbox.clientsClaim();
